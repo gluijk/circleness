6036ae3da96e4a1c744b1058ce114e23eff1ebc1
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2022-09-15 23:34:39 +0200

    Add files via upload

--- circleness.R
+++ circleness.R
@@ -15,13 +15,13 @@ NSTONES=5
 # dcraw -v -r 1 1 1 1 -o 0 -4 -T *.arw (except stone 4)
 for (i in 1:NSTONES) {
     img=readTIFF(paste0(NAME, i, ".tiff"), native=FALSE, convert=FALSE)
-    img=0.299*img[,,1]+0.587*img[,,1]+0.114*img[,,1]  # average channels
+    img=0.299*img[,,1]+0.587*img[,,2]+0.114*img[,,3]  # average channels
     img=img-min(img)  # normalize to 0..1
     img=img/max(img)
     img=img^(1/2.2)  # 2.2 gamma curve
-    hist(img, breaks=512)
+    hist(img, breaks=1024)
     
-    TH=0.35  # 0.2  # black/white threshold
+    TH=0.35  # 0.2  # black/white thresholds
     img[img<TH]=0
     img[img>=TH]=1
     
@@ -49,13 +49,15 @@ for (i in 1:NSTONES) {
     for (j in 0:(N-1)) {
         theta=j*2*pi/N
         r=1
-        x1=x0+r*cos(theta)
-        y1=y0+r*sin(theta)
+        costheta=cos(theta)
+        sintheta=sin(theta)
+        x1=x0+r*costheta
+        y1=y0+r*sintheta
         
         while (img[round(x1), round(y1)]==1) {
             r=r+0.5
-            x1=x0+r*cos(theta)
-            y1=y0+r*sin(theta)           
+            x1=x0+r*costheta
+            y1=y0+r*sintheta          
         }
         R=c(R,r)
     }
@@ -64,7 +66,7 @@ for (i in 1:NSTONES) {
     Rmin=min(R)  # min radius in pixels
     Rmax=max(R)  # max radius in pixels
 
-    R=R/Rmean  # normalize by mean(R)
+    R=R/Rmean  # normalize R
     
     # Plot R/mean(R) for all the stones
     if (i==1) {
@@ -79,10 +81,10 @@ for (i in 1:NSTONES) {
     VarR=mean((R-mean(R))^2)  # total variance
     circleness=exp(-k*VarR)  # scaling circleness to 1(=perfect circle)..0
     circle=c(circle, circleness)
-    print(paste0("Circle ", i, ": ", circleness))    
+    print(paste0("Stone ", i, ": ", circleness))    
     
     
-    # Read bitmaps and implode stones from their max radius
+    # Read bitmaps and implode stones from max radius
     img=readTIFF(paste0(NAME, i, ".tif"), native=FALSE, convert=FALSE)
     img2=img*0+1
     
@@ -96,6 +98,7 @@ for (i in 1:NSTONES) {
         }
     }
 
+    # Draw centre os mass axes and max radius circle
     img[x0,,]=0
     img[,y0,]=0
     for (j in 0:(N-1)) {
